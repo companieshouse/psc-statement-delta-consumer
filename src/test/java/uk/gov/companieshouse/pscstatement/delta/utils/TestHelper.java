fd34341f06b69c8bb27501dcf3039903b7e77b26
JAndrewCH <jandrew@companieshouse.gov.uk>
2023-01-25 11:27:23 +0000

    add processor tests

--- src/test/java/uk/gov/companieshouse/pscstatement/delta/utils/TestHelper.java
+++ src/test/java/uk/gov/companieshouse/pscstatement/delta/utils/TestHelper.java
@@ -1,5 +1,8 @@
 package uk.gov.companieshouse.pscstatement.delta.utils;
 
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.util.FileCopyUtils;
 import uk.gov.companieshouse.delta.ChsDelta;
 
@@ -14,11 +17,31 @@ public class TestHelper {
 
         return buildDelta(data);
     }
+
+    public Message<ChsDelta> createChsDeltaMessage() throws IOException {
+        InputStreamReader exampleJsonPayload = new InputStreamReader(
+                ClassLoader.getSystemClassLoader().getResourceAsStream("psc-statement-delta-example.json"));
+        String data = FileCopyUtils.copyToString(exampleJsonPayload);
+
+        return buildMessage(data);
+    }
+
+    public Message<ChsDelta> createInvalidChsDeltaMessage() {
+        return buildMessage("This is some invalid data");
+    }
+
     private ChsDelta buildDelta(String data) {
         return ChsDelta.newBuilder()
                 .setData(data)
                 .setContextId("8Ch2wOs16s5Yqxl2vX42n7GwNHT4")
                 .setAttempt(0)
+                .build();
+    }
+    private Message<ChsDelta> buildMessage (String data) {
+        return MessageBuilder
+                .withPayload(buildDelta(data))
+                .setHeader(KafkaHeaders.RECEIVED_TOPIC, "test")
+                .setHeader("PSC_STATEMENT_DELTA_RETRY_COUNT", 1)
                 .build();
     }
 }
