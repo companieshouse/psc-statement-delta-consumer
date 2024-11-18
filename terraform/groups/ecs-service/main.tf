aac8b424f901e858d3e553304ae090d1bba4eb8b
sthompsonCH <sthompson@companieshouse.gov.uk>
2024-11-18 15:16:40 +0000

    DSND-3184: Remove unnecessary line

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -29,7 +29,6 @@ module "secrets" {
 
 module "ecs-service" {
   source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.292"
-  read_only_root_filesystem = false
 
   # Environmental configuration
   environment             = var.environment
