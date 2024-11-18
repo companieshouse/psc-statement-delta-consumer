80b604554685b31cd48f34f5f187ab1b3e240cfa
sthompsonCH <105206260+sthompsonCH@users.noreply.github.com>
2024-11-18 17:03:36 +0000

    DSND-3184: Add flag to fix cidev apply job (#51)

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -29,6 +29,7 @@ module "secrets" {
 
 module "ecs-service" {
   source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.292"
+  read_only_root_filesystem = false
 
   # Environmental configuration
   environment             = var.environment
