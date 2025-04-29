24bb59165640bdecf2aa5ec1ce69859993b12bbd
nghessen-ch <nghessen@companieshouse.gov.uk>
2025-04-29 14:48:52 +0100

    adding values for hc config

--- terraform/groups/ecs-service/variables.tf
+++ terraform/groups/ecs-service/variables.tf
@@ -129,17 +129,21 @@ variable "psc_statement_delta_consumer_version" {
 variable "task_healthcheck_interval" {
   type        = number
   description = "Health check interval configuration for ECS task definitions."
+  default     = 60
 }
 variable "task_healthcheck_timeout" {
   type        = number
   description = "Health check timeout configuration for ECS task definitions."
+  default     = 5
 }
 variable "task_healthcheck_retries" {
   type        = number
   description = "Health check retries configuration for ECS task definitions."
+  default     = 3
 }
 
 variable "task_healthcheck_start_period" {
   type        = number
   description = "Health check start period configuration for ECS task definitions."
+  default     = 120
 }
