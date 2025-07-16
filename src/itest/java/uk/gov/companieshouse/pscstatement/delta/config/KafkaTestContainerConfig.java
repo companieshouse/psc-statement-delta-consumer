14e846b0deccdab32d8d99d67d08d60ac4e61d4f
yshah-ch <yshah@companieshouse.gov.uk>
2025-07-16 18:57:33 +0100

    initial commit

--- src/itest/java/uk/gov/companieshouse/pscstatement/delta/config/KafkaTestContainerConfig.java
+++ src/itest/java/uk/gov/companieshouse/pscstatement/delta/config/KafkaTestContainerConfig.java
@@ -22,7 +22,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
-import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.kafka.ConfluentKafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 import uk.gov.companieshouse.delta.ChsDelta;
 
@@ -40,8 +40,8 @@ public class KafkaTestContainerConfig {
     }
 
     @Bean
-    public KafkaContainer kafkaContainer() {
-        KafkaContainer kafkaContainer = new KafkaContainer(
+    public ConfluentKafkaContainer kafkaContainer() {
+        ConfluentKafkaContainer kafkaContainer = new ConfluentKafkaContainer(
                 DockerImageName.parse("confluentinc/cp-kafka:latest"));
         kafkaContainer.start();
         return kafkaContainer;
@@ -64,7 +64,7 @@ public class KafkaTestContainerConfig {
     }
 
     @Bean
-    public Map<String, Object> consumerConfigs(KafkaContainer kafkaContainer) {
+    public Map<String, Object> consumerConfigs(ConfluentKafkaContainer kafkaContainer) {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers());
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
@@ -78,16 +78,15 @@ public class KafkaTestContainerConfig {
     }
 
     @Bean
-    public ProducerFactory<String, Object> producerFactory(KafkaContainer kafkaContainer) {
+    public ProducerFactory<String, Object> producerFactory(ConfluentKafkaContainer kafkaContainer) {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, AvroDeserializer.class);
         props.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, TopicErrorInterceptor.class.getName());
-        DefaultKafkaProducerFactory<String, Object> factory = new DefaultKafkaProducerFactory<>(
-                props, new StringSerializer(), serializer);
 
-        return factory;
+        return new DefaultKafkaProducerFactory<>(
+                props, new StringSerializer(), serializer);
     }
 
     @Bean
